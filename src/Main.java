8b23353378e96f148b83da8dc53e686997fe9266
aliaksandr.zelianko <aliaksandr.zelianko@rdigital.by>
2022-06-08 12:24:05 +0300

    delete all

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import java.time.format.DateTimeFormatterBuilder;
 
 public class Main {
     public static void main(String[] args) {
-        sout("Hello ");
-        String text = String.format("Почтовое отправление %s вручено %s", "SR123123123123BY", LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-        System.out.println(text);
+
     }
 }
