0cba62d3807d67f07b0ead9a6fd2b50ac458af4e
aliaksandr.zelianko <aliaksandr.zelianko@rdigital.by>
2022-06-08 12:29:21 +0300

    commit 4 main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         System.out.println(LocalDate.now());
-
-
+        System.out.println(LocalDate.now());
+        System.out.println(LocalDate.now());
+        System.out.println(LocalDate.now());
+        System.out.println(LocalDate.now());
+        System.out.println(LocalDate.now());
     }
 }
