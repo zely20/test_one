4419d22cfe2456de4636c7f404d4df752e8fc4fd
aliaksandr.zelianko <aliaksandr.zelianko@rdigital.by>
2022-06-08 12:17:55 +0300

    second change

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatterBuilder;
 public class Main {
     public static void main(String[] args) {
         String text = String.format("Почтовое отправление %s вручено %s", "SR123123123123BY", LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        System.out.println(LocalDate.now());
         System.out.println(text);
     }
 }
